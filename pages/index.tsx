ece5b59fb612c53d2bb40bfd2f222b10c1de6719
Eesa Zahed <eszhd1@gmail.com>
2025-01-17 18:22:21 -0500

    major site updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,45 +12,69 @@ const Home: NextPage = () => {
   }, []);
 
   return (
-    <div className="mx-8">
+    <div>
       <Head>
         <title>SenseNow Academy</title>
         <link
           className="mb-4"
           rel="shortcut icon"
-          href="https://gist.github.com/user-attachments/assets/4aab369f-1ba7-4e03-9e6a-dc1d7c94213a.png"
+          href="https://gist.github.com/user-attachments/assets/f2bb70c5-c62a-4dff-a50c-86bacaabb6bd.png"
           type="image/x-icon"
         />
       </Head>
-      <main className="px-8 w-full md:w-1/2 mx-auto md:text-xl">
-        <div className="px-8 mx-auto w-full md:w-2/3 my-16">
+
+      <header
+        className="w-full py-16"
+        style={{
+          backgroundImage:
+            "url(https://images.pexels.com/photos/1181675/pexels-photo-1181675.jpeg)",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+        }}
+      >
+        <div className="m-auto w-72 h-72">
           <img
-            src="https://gist.github.com/user-attachments/assets/4aab369f-1ba7-4e03-9e6a-dc1d7c94213a.png"
-            className="w-full"
+            src="https://gist.github.com/user-attachments/assets/f2bb70c5-c62a-4dff-a50c-86bacaabb6bd.png"
+            className="w-full h-full"
             alt="Logo for SenseNow Academy"
           />
         </div>
+      </header>
 
+      <div className="mx-8">
+        <a
+          href="https://example.com"
+          target="_blank"
+          rel="noreferrer"
+          className="w-full mx-auto md:w-1/3 font-sans uppercase block bg-green-500 hover:bg-green-600 text-white no-underline py-4 my-16 rounded-xl text-2xl text-center cursor-pointer duration-200"
+        >
+          Register now
+        </a>
+      </div>
+
+      <main className="rounded-2xl p-12 my-12 w-full md:w-1/2 mx-auto md:text-xl md:shadow-2xl">
         {/* <h1 className="text-center my-8 text-4xl md:text-7xl font-bold bg-gradient-to-r from-[#b800ff] to-[#d4fc00] bg-clip-text text-transparent py-4">
           Coding is important
         </h1> */}
 
-        <div className="pb-24">
+        <div>
           <section className="mb-12">
-            <h2 className="md:text-left text-center font-bold text-4xl md:text-6xl mb-4">
+            <h2 className="md:text-left text-center font-bold text-4xl md:text-6xl mb-8">
               Coding is cruical
             </h2>
             <p>
               The Ontario Ministry of Education&apos;s{" "}
               <a
-                href="https://www.dcp.edu.gov.on.ca/en/curriculum/science-technology/context/strands#strand-a"
+                href="https://www.dcp.edu.gov.on.ca/en/curriculum/science-technology/grades/grade-1/a/a2"
                 target="_blank"
                 rel="noreferrer"
               >
                 updated curriculum
               </a>{" "}
-              now includes coding in grades as low as Grade 2 and continues
-              throughout high school as of 2022.
+              now includes coding in grades{" "}
+              <span className="text-[red] font-bold">as low as Grade 1</span>
+              and continues throughout high school as of 2022.
             </p>
             <br />
             <p>
@@ -69,12 +93,17 @@ const Home: NextPage = () => {
             </p>
             <br />
             <p>
-              Interested?{" "}
-              <a href="https://example.com" target="_blank" rel="noreferrer">
-                Book me
-              </a>{" "}
-              and get the journey started!
+              Interested? This is a limited offer, there&apos;s no time to
+              waste.
             </p>
+            <a
+              href="https://example.com"
+              target="_blank"
+              rel="noreferrer"
+              className="w-full mx-auto md:w-1/3 font-sans uppercase block bg-green-500 hover:bg-green-600 text-white no-underline py-4 mt-8 mb-16 rounded-xl text-2xl text-center cursor-pointer duration-200"
+            >
+              Register now
+            </a>
           </section>
 
           <section className="mb-12">
@@ -116,10 +145,9 @@ const Home: NextPage = () => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  {" "}
                   freeCodeCamp.org
                 </a>
-              </li>{" "}
+              </li>
               <li className="mb-4">
                 Built a fully functioning{" "}
                 <a
