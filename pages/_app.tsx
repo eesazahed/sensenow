e45760be60c13662b7fd84d5eb29233a2a5c0bea
Eesa Zahed <eszhd1@gmail.com>
2025-01-18 14:30:10 -0500

    analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,12 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Analytics />
+    <Component {...pageProps} />
+  </>
 );
 
 export default App;
